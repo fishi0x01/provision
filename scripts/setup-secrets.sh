de92a899a3d00e7c41c8946f1e696682b2106c04
Karl Fischer <fishi0x01@gmail.com>
2019-05-21 14:23:07 +0200

    Add jira config to secrets setup

--- scripts/setup-secrets.sh
+++ scripts/setup-secrets.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-pass show fishi0x01/Automation/IRSSI/config > ~/.irssi/config
+pass show fishi0x01/Automation/IRSSI/config 2>/dev/null > ~/.irssi/config
 chmod 600 ~/.irssi/config
 
-pass show fishi0x01/Automation/ssh-ident/config > ~/.ssh-ident
+pass show fishi0x01/Automation/ssh-ident/config 2>/dev/null > ~/.ssh-ident
 chmod 640 ~/.ssh-ident
+
+mkdir -p ~/.jira.d/
+pass show fishi0x01/data4life/jira/config.yml 2>/dev/null > ~/.jira.d/config.yml
+chmod -R 644 ~/.jira.d/
