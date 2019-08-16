7cc1c8fa4cfcc7322acf57dce7d540d3a528f7ca
Karl Fischer <fishi0x01@gmail.com>
2019-08-16 17:16:31 +0200

    Release

--- fetch-and-install.sh
+++ fetch-and-install.sh
@@ -3,7 +3,7 @@
 ## For security reasons it is prefered though to copy/paste and check locally before executing it
 
 { # Prevent execution if this script was only partially downloaded
-  export FISHI_VERSION="2019-01-04";
+  export FISHI_VERSION="2019-08-16";
   curl -sL "https://github.com/fishi0x01/local-env/releases/download/${FISHI_VERSION}/${FISHI_VERSION}_fishi-env.tar.gz" -o "${FISHI_VERSION}_fishi-env.tar.gz"; 
   curl -sL "https://github.com/fishi0x01/local-env/releases/download/${FISHI_VERSION}/SHA256SUMS" -o "SHA256SUMS"; 
   sha256sum -c SHA256SUMS || exit 1; 
