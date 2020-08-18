d664ee8164c83a1389f35134dfd30387db415047
Karl Fischer <fishi0x01@gmail.com>
2020-08-18 12:17:23 +0200

    doc

--- nix/installer/install-nix.sh
+++ nix/installer/install-nix.sh
@@ -8,6 +8,8 @@ source ${CUR_DIR}/settings.sh
 # this is helpful in case keyservers are down
 gpg --import ${CUR_DIR}/install-nix/nix-edolstra.asc
 
+# Atlernatively pull keys from public server
 #gpg --recv-keys ${NIX_GPG_KEY}
+
 gpg --verify ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}.asc ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}
 sh ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}
