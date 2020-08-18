a223aaf982f381f8eb9e421c3d7e0029ebe456e7
Karl Fischer <fishi0x01@gmail.com>
2020-08-18 12:03:57 +0200

    nix 2.3.7

--- nix/installer/settings.sh
+++ nix/installer/settings.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export NIX_VERSION=2.3.6
+# https://nixos.org/download.html
+export NIX_VERSION=2.3.7
 export NIX_GPG_KEY=B541D55301270E0BCF15CA5D8170B4726D7198DE
 export NIX_DEFAULT_ENV=fishi0x01-minimal
 export NIX_CHANNEL=nixos-20.03
