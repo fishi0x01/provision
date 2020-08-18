d28cfe9823cc0d94c692987ec4156727a6453b43
Karl Fischer <fishi0x01@gmail.com>
2020-08-18 12:14:47 +0200

    more keys

--- nix/installer/install-nix.sh
+++ nix/installer/install-nix.sh
@@ -3,6 +3,11 @@ set -e
 
 CUR_DIR=$(dirname $(readlink -f "$0"))
 source ${CUR_DIR}/settings.sh
-gpg --recv-keys ${NIX_GPG_KEY}
+
+# https://github.com/NixOS/nixos-homepage/blob/master/edolstra.gpg
+# this is helpful in case keyservers are down
+gpg --import ${CUR_DIR}/install-nix/nix-edolstra.asc
+
+#gpg --recv-keys ${NIX_GPG_KEY}
 gpg --verify ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}.asc ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}
 sh ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}
