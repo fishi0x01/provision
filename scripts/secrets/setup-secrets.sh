857bd06d81b3a4c714bae40322277b70b6c514a4
Karl Fischer <fishi0x01@gmail.com>
2020-09-04 14:04:56 +0200

    Ubuntu 20.04

--- scripts/secrets/setup-secrets.sh
+++ scripts/secrets/setup-secrets.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+mkdir -p ~/.irssi/
 pass show fishi0x01/Automation/IRSSI/config 2>/dev/null > ~/.irssi/config
-chmod 600 ~/.irssi/config
+chmod 700 ~/.irssi/config
 
 pass show fishi0x01/Automation/ssh-ident/config 2>/dev/null > ~/.ssh-ident
 chmod 640 ~/.ssh-ident
 
 mkdir -p ~/.jira.d/
 pass show fishi0x01/data4life/jira/config.yml 2>/dev/null > ~/.jira.d/config.yml
-chmod -R 644 ~/.jira.d/
+chmod -R 700 ~/.jira.d/
