a092b44a52b3d0d4f007022cd3eed318bacb9f46
Karl Fischer <fishi0x01@gmail.com>
2021-11-26 14:25:26 +0100

    Update nix

--- nix/installer/settings.sh
+++ nix/installer/settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://nixos.org/download.html
-export NIX_VERSION=2.3.15
+export NIX_VERSION=2.4
 export NIX_GPG_KEY=B541D55301270E0BCF15CA5D8170B4726D7198DE
 export NIX_DEFAULT_ENV=fishi0x01-minimal
 export NIX_CHANNEL=nixos-21.05
