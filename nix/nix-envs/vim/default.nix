5063c56786cd1c694da918bd9fcc9793d379f91e
Karl Fischer <fishi0x01@gmail.com>
2022-11-23 21:04:20 +0100

    deprecate vam

--- nix/nix-envs/vim/default.nix
+++ nix/nix-envs/vim/default.nix
@@ -1,40 +1,29 @@
-{pkgs, vim_configurable}:
-let
-  ### vim-virtualenv
-  customPlugins.vim-virtualenv = pkgs.vimUtils.buildVimPlugin {
-    name = "vim-virtualenv";
-    src = pkgs.fetchFromGitHub {
-      owner = "jmcantrell";
-      repo = "vim-virtualenv";
-      rev = "e7bb5b1960603decf2975f052db672850c5a7de4";
-      sha256 = "0fy3w6kmmvhqk54f428rvydw3cs13iv8dalk30mazfjzbnq4ckfq";
-    };
-  };
-in
+{ pkgs, vim_configurable }:
 {
   pkg = vim_configurable.customize { 
-    name = "vim"; 
+    name = "vim";
     vimrcConfig.customRC = builtins.readFile ./.vimrc;
-    vimrcConfig.vam.knownPlugins = pkgs.vimPlugins // customPlugins;
-    vimrcConfig.vam.pluginDictionaries = [
-      { names = [
-        "ansible-vim"
-        "nerdtree"
-        "kotlin-vim"
-        "ctrlp-vim"
-        "YouCompleteMe"
-        "rust-vim"
-        "syntastic"
-        "vimux"
-        "vim-fugitive"
-        "vim-clang-format"
-        "vim-nix"
-        "vim-json"
-        "vim-jinja"
-        "vim-jsx-pretty"
-        "vim-toml"
-        "vim-vagrant"
-      ];}
-    ];
+    vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
+      start = [
+        ansible-vim
+        nerdtree
+        kotlin-vim
+        ctrlp-vim
+        YouCompleteMe
+        rust-vim
+        syntastic
+        vimux
+        vim-fugitive
+        vim-clang-format
+        vim-nix
+        vim-json
+        vim-jinja
+        vim-jsx-pretty
+        vim-toml
+        vim-vagrant
+      ];
+      opt = [];
+    };
   };
 }
+
