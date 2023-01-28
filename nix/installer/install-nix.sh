6db539c3b051c1f9ae35432428d03bca206de5ef
Karl Fischer <fishi0x01@users.noreply.github.com>
2023-01-28 15:19:10 +0100

    updates (#5)

--- nix/installer/install-nix.sh
+++ nix/installer/install-nix.sh
@@ -6,10 +6,12 @@ source ${CUR_DIR}/settings.sh
 
 # https://github.com/NixOS/nixos-homepage/blob/master/edolstra.gpg
 # this is helpful in case keyservers are down
-gpg --import ${CUR_DIR}/install-nix/nix-edolstra.asc
+# -> Since 2.13.0 no gpg signature is provided
+#gpg --import ${CUR_DIR}/install-nix/nix-edolstra.asc
 
 # Atlernatively pull keys from public server
 #gpg --recv-keys ${NIX_GPG_KEY}
 
-gpg --verify ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}.asc ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}
+# -> Since 2.13.0 no gpg signature is provided
+#gpg --verify ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}.asc ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}
 sh ${CUR_DIR}/install-nix/install-nix-${NIX_VERSION}
