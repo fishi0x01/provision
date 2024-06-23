2226b7e213b4fdb26f5fcb1d303600bd40b7c99a
Karl Fischer <fishi0x01@gmail.com>
2024-06-23 11:55:37 +0200

    update nix channel

--- nix/installer/settings.sh
+++ nix/installer/settings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # https://nixos.org/download.html
-export NIX_VERSION=2.17.0
+export NIX_VERSION=2.18.2
 export NIX_DEFAULT_ENV=fishi0x01-minimal
-export NIX_CHANNEL=nixos-23.05
+export NIX_CHANNEL=nixos-24.05
 
 # -> Since 2.13.0 no gpg signature is provided
 #export NIX_GPG_KEY=B541D55301270E0BCF15CA5D8170B4726D7198DE
