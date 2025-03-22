bae87b17c4fbfec10631720539746bc1e99a2349
Karl Fischer <fishi0x01@gmail.com>
2025-03-22 01:55:40 +0100

    fix tests

--- nix/installer/settings.sh
+++ nix/installer/settings.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # https://nixos.org/download.html
-export NIX_VERSION=2.23.0
 export NIX_DEFAULT_ENV=fishi0x01-minimal
 export NIX_CHANNEL=nixos-24.05
 
-# -> Since 2.13.0 no gpg signature is provided
-#export NIX_GPG_KEY=B541D55301270E0BCF15CA5D8170B4726D7198DE
